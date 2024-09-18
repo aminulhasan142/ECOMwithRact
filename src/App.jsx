cd876bcf75ff868ac84f9e1863de012902ab9fd5
aminulhasan142 <mahshoukhin142@gmail.com>
2024-09-18 22:36:04 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline md:text-cyan-800 text-center lg:text-black">
+      Hello world!
+    </h1>
+  );
 }
