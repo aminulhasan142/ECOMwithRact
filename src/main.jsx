0a493c7d672f29b4d3e70e7ff2658eea4404e77c
aminulhasan142 <mahshoukhin142@gmail.com>
2024-09-19 20:07:51 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Header from "./pages/Header.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header />
+  </StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      black: "#000F06",
+      "008738": "#008738",
+      "color-green-800": "#166534",
+      "color-green-900": "#14532D",
+      "color-green-950": "#052E16",
+      "color-green-600": "#16A34A",
+      "color-green-500": "#22C55E",
+      "color-green-50": "#F0FDF4",
+      "color-base-white": "#14532D",
+      "color-base-black": "#000000",
+      "color-neutral-900": "#171717",
+    },
+    fontFamily: {
+      maven: ["Maven Pro"], // Adds a new `font-display` class
+      poppins: ["Poppins"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      flex: {
+        100: "1 0 0%",
+      },
+    },
   },
   plugins: [],
 };
