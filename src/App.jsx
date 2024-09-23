caa299f61d23a2f6d2631095eb5471ea9b4d987b
aminulhasan142 <mahshoukhin142@gmail.com>
2024-09-23 16:58:16 +0600

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./pages/Navbar/Navbar";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline md:text-cyan-800 text-center lg:text-black">
-      Hello world!
+    <h1>
+      <Navbar />
     </h1>
   );
 }
